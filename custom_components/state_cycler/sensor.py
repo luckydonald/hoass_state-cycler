1fd2ff5f85fe9d8e9f5064831d58e66e9de6f9a1
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-18 03:34:21 +0100

    ✨ ai: [004] running… (1/X)

--- custom_components/state_cycler/sensor.py
+++ custom_components/state_cycler/sensor.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
