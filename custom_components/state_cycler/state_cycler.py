c9e4234859dd30799cf1f1c3719187048faaef99
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-19 01:30:18 +0100

    ✨ ai: [010] Add `PLATFORM_SCHEMA` (1/1)

--- custom_components/state_cycler/state_cycler.py
+++ custom_components/state_cycler/state_cycler.py
@@ -50,6 +50,14 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORM_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_STATES): vol.All(list, [str]),
+        vol.Optional(CONF_INCLUDE_OFF_STATE, default=DEFAULT_INCLUDE_OFF_STATE): bool,
+        vol.Optional(CONF_TIMER_INTERVAL): vol.All(vol.Coerce(float), vol.Range(min=0.1)),
+    }
+)
+
 
 async def async_setup_platform(
     hass: HomeAssistant,
