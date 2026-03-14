efd76d2f349937c55d7477a4c70f66f2328c712c
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-03-14 01:04:19 +0100

    🫥 Remove unnecessary import.

--- custom_components/state_cycler/__init__.py
+++ custom_components/state_cycler/__init__.py
@@ -6,7 +6,6 @@ import logging
 from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_send
